e46eb065fc8fc07fc5224533fcc49eb44bd26c73
thosou <thomas.souvignet@gmx.fr>
2019-06-20 16:19:14 +0200

    put some comments

--- divider_by_2/simulation.py
+++ divider_by_2/simulation.py
@@ -2,23 +2,26 @@ import matplotlib.pyplot as plt
 from divider_by_2.matrices import *
 from control import forced_response
 
-
+# duration of each phase
 d1 = 1e-8
 d2 = 1e-8
-points = 10
-nb_period = 200
+points = 10  # number of points per phase
+nb_period = 100  # number of period to simulate
 
-
+# create the time vector for each phase
 t1 = np.linspace(0, d1, points)
 t2 = np.linspace(0, d2, points)
 
+# create the input vector for each phase
 u1 = np.array([np.ones_like(t1) * ILOAD, np.ones_like(t1) * VIN])
 u2 = np.array([np.ones_like(t2) * ILOAD, np.ones_like(t2) * VIN])
 
-t = np.zeros((1, 1))
-x = np.zeros((1, 2))
-y = np.zeros((1, 1))
+# initialization of vectors
+t = np.zeros((1,))
+x = np.zeros((2, 1))
+y = np.zeros((1,))
 
+# hybrid system simulation
 for n in range(nb_period):
     t_1, y_1, x_1 = forced_response(sys=phase_1, T=t1 + t[-1], U=u1, X0=x[:, -1])
     t = np.concatenate((t, t_1), axis=0)
@@ -29,6 +32,8 @@ for n in range(nb_period):
     x = np.concatenate((x, x_2), axis=1)
     y = np.concatenate((y, y_2), axis=0)
 
+# plot the state
 plt.figure()
 plt.plot(t, x[0], t, x[1])
+plt.plot(t, y)
 plt.show()
