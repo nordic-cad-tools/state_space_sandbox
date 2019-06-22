0ae35ccd474eea8ff1c2e7b3fe744a81394b9da4
thosou <thomas.souvignet@gmx.fr>
2019-06-22 20:49:20 +0200

    black formatting

--- divider_by_2/find_steady_state.py
+++ divider_by_2/find_steady_state.py
@@ -8,11 +8,11 @@ def find_phase_duration(phases, E, set_point):
     phase_1, phase_2 = phases
 
     def fun(d, E, set_point):
-        return (
-            dot(E, fixed_point([phase_1, phase_2], [d[0], d[0]])) - set_point
-        )
+        return dot(E, fixed_point([phase_1, phase_2], [d[0], d[0]])) - set_point
 
-    d, info, ier, msg = fsolve(fun, 1e-8, full_output=True, args=(E, set_point),xtol=1e-12)
+    d, info, ier, msg = fsolve(
+        fun, 1e-8, full_output=True, args=(E, set_point), xtol=1e-12
+    )
     if ier == 1:
         return d[0]
     else:
