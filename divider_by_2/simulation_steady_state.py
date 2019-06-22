7f30926e104dbcbe29787b489feff98da59ad1fd
thosou <thomas.souvignet@gmx.fr>
2019-06-22 19:55:44 +0200

    added steady state verification

---
+++ divider_by_2/simulation_steady_state.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from divider_by_2.matrices import *
+from control import forced_response
+from divider_by_2.fixed_point import fixed_point
+from divider_by_2.find_steady_state import find_phase_duration
+
+# duration of each phase
+d1 = d2 = find_phase_duration([phase_1, phase_2], E= np.array([0,1]), set_point=0.7)
+points = 100  # number of points per phase
+nb_period = 10  # number of period to simulate
+
+# create the time vector for each phase
+t1 = np.linspace(0, d1, points)
+t2 = np.linspace(0, d2, points)
+
+# create the input vector for each phase
+u1 = np.array([np.ones_like(t1) * ILOAD, np.ones_like(t1) * VIN])
+u2 = np.array([np.ones_like(t2) * ILOAD, np.ones_like(t2) * VIN])
+
+# initialization of vectors
+t = np.zeros((1,))
+x = fixed_point([phase_1, phase_2], [d1, d2])
+y = np.dot(np.array([0, 1]), x)
+
+# hybrid system simulation
+for n in range(nb_period):
+    t_1, y_1, x_1 = forced_response(sys=phase_1, T=t1 + t[-1], U=u1, X0=x[:, -1])
+    t = np.concatenate((t, t_1), axis=0)
+    x = np.concatenate((x, x_1), axis=1)
+    y = np.concatenate((y, y_1), axis=0)
+    t_2, y_2, x_2 = forced_response(sys=phase_2, T=t2 + t[-1], U=u2, X0=x_1[:, -1])
+    t = np.concatenate((t, t_2), axis=0)
+    x = np.concatenate((x, x_2), axis=1)
+    y = np.concatenate((y, y_2), axis=0)
+
+# plot the state
+plt.figure()
+plt.plot(t, x[0], t, x[1])
+plt.plot(t, y)
+# plt.plot(t[-1], x_0[0], "o")
+# plt.plot(t[-1], x_0[1], "o")
+
+plt.show()
