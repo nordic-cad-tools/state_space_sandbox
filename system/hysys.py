9be2fdbb35ef25606ad5ee4107950e5453eaccb7
thosou <thomas.souvignet@gmx.fr>
2019-07-28 12:56:35 +0200

    added phi calculation

--- system/hysys.py
+++ system/hysys.py
@@ -6,7 +6,8 @@ from control import forced_response
 import matplotlib.pyplot as plt
 
 import numpy as np
-
+from numpy import dot, eye, trapz, linspace, apply_along_axis, round
+from scipy.linalg import expm, det, inv
 
 class ModeMatrix:
     def __init__(self, elements, expression):
@@ -41,13 +42,16 @@ class ModeMatrix:
         """
         return self.symbol.subs(parameters)
 
+    def to_array(self, parameters):
+        return np.array(self.eval(parameters)).astype(np.float64)
 
 class Mode:
-    def __init__(self, A, B, C, D):
+    def __init__(self, A, B, C, D, duration):
         self.A = ModeMatrix(**A)
         self.B = ModeMatrix(**B)
         self.C = ModeMatrix(**C)
         self.D = ModeMatrix(**D)
+        self.duration = duration
 
     @classmethod
     def from_json(cls, data):
@@ -55,19 +59,21 @@ class Mode:
 
     def to_json(self):
         return {
+            "duration": self.duration,
             "A": self.A.to_json(),
             "B": self.B.to_json(),
             "C": self.C.to_json(),
             "D": self.D.to_json(),
         }
 
-    def to_ss(self, parameters):
-        return StateSpace(
-            np.array(self.A.eval(parameters)).astype(np.float64),
+    def matrices(self, parameters):
+        return [np.array(self.A.eval(parameters)).astype(np.float64),
             np.array(self.B.eval(parameters)).astype(np.float64),
             np.array(self.C.eval(parameters)).astype(np.float64),
-            np.array(self.D.eval(parameters)).astype(np.float64),
-        )
+            np.array(self.D.eval(parameters)).astype(np.float64)]
+
+    def to_ss(self, parameters):
+        return StateSpace(*self.matrices(parameters))
 
 
 class HySy:
@@ -92,10 +98,10 @@ class HySy:
             "modes": [mode.to_json() for mode in self.modes],
         }
 
-    def transient(self, switching_instants, nb_periods, inputs=[], nb_points=10):
+    def transient(self, durations, nb_periods, inputs=[], nb_points=10):
 
         # create the time vector for each phase
-        t_vectors = [np.linspace(0, d, nb_points) for d in switching_instants]
+        t_vectors = [np.linspace(0, d, nb_points) for d in durations]
         # create the input vector for each phase
         u_vectors = [np.array([np.ones_like(t) * i for i in inputs]) for t in t_vectors]
         # initialization of vectors
@@ -119,6 +125,66 @@ class HySy:
 
         return t, x, y
 
+    def fixed_point(self, durations, inputs=[]):
+        matrices = [mode.matrices(self.parameters) for mode in self.modes]
+
+        A1, B1, C1 ,D1 = self.modes[0].matrices(self.parameters)
+        A2, B2, C2 ,D2 = self.modes[1].matrices(self.parameters)
+
+        # phi calculation
+        phi = 1
+        for mode in self.modes:
+            phi = dot(expm(mode.A.to_array(self.parameters) * mode.duration), phi)
+
+        print(phi)
+        # tempory soution for 2 phases system
+        d1, d2 = durations
+        # if Ai are not singular
+        # used scipy instead of numpy, it gives ad results. Also need to use np.array and not matrix
+        det_A1 = round(det(A1))
+        det_A2 = round(det(A2))
+
+        # Identity matrix
+        n, m = A1.shape
+        I = eye(n, m)
+
+        # duration of each phase
+        # d1 = 1e-8
+        # d2 = 1e-8
+
+        # phi calculation
+        phi_1 = expm(A1 * d1)
+        phi_2 = expm(A2 * d2)
+        phi = dot(phi_2, phi_1)
+        print(phi)
+        # calculate the integral of exp matrix
+        def expm_int(A, T, pt=100):
+
+            def f(A, t):
+                return expm(A * t)
+
+            t = linspace(0, T, pt)
+            result = apply_along_axis(f, 0, t.reshape(1, -1), A)
+            return trapz(result, t)
+
+        # gamma calculation
+        # check for singularity
+        if all([det_A1, det_A2]):
+            # non singular matrix
+            gam_1 = dot(dot((phi_1 - I), inv(A1)), B1)
+            gam_2 = dot(dot((phi_2 - I), inv(A2)), B2)
+        else:
+            # singular matrix
+            gam_1 = dot(expm_int(A1, d1), B1)
+            gam_2 = dot(expm_int(A2, d2), B2)
+
+        gam = dot(phi_2, gam_1) + gam_2
+
+        # fixed point calculation
+        X0 = dot(dot(inv(I - phi), gam), inputs)
+        return X0
+
+
 if __name__ == "__main__":
     # load json file into sys
     with open("system.json", "r") as f:
@@ -129,12 +195,17 @@ if __name__ == "__main__":
     # print(my_sys.modes[0].D.eval(my_sys.parameters))
     # print(my_sys.modes[0].to_ss(my_sys.parameters))
 
-    t, x, y = my_sys.transient(switching_instants=[1e-8, 1e-8], nb_periods=50, inputs=[5e-3, 2])
-    plt.figure()
-    plt.plot(t, x[0], t, x[1])
-    plt.plot(t, y)
-    plt.show()
+    U = np.array([[5e-3], [2]])
 
+    x0 = my_sys.fixed_point([1e-8, 1e-8], inputs=U)
+    # print(x0)
+    # t, x, y = my_sys.transient(durations=[1e-8, 1e-8], nb_periods=50, inputs=[5e-3, 2])
+    # plt.figure()
+    # plt.plot(t, x[0], t, x[1])
+    # plt.plot(t, y)
+    # plt.plot(t[-1], x0[0], "o")
+    # plt.plot(t[-1], x0[1], "o")
+    # plt.show()
     # MODE = {
     #     "A": {
     #         "elements": {
